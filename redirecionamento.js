c455fbfe877108e7fb28f3f47cf7a83e995480a2
joaquim-gomes <146651316+joaquim-gomes@users.noreply.github.com>
2023-12-20 12:15:28 -0300

    Add files via upload

---
+++ redirecionamento.js
@@ -0,0 +1,19 @@
+function carta() {
+       window.location.href ='indexp.html';
+} 
+
+function capitulo1() {
+    window.location.href = 'index1.html'
+}
+
+function capitulo2() {
+    window.location.href = 'index2.html'
+}
+
+function capitulo3() {
+    window.location.href = 'index3.html'
+}
+
+function inicial() {
+    window.location.href = 'indexi.html'
+}
